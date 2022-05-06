b56c5002cb216dc7c33db90c0c8375a35f006619
Steve <montyblancs99@gmail.com>
2022-05-06 11:47:02 -0400

    Adds a stattracker config file

---
+++ IOconfig.lua
@@ -0,0 +1,40 @@
+freeslot("sfx_stcrea", "sfx_stload")
+local loaded = false
+
+-- store commands, we'll iterate through this to create a config if necessary
+local commands = {
+	{sTrack.cv_recordpopup, "Shows/Hides record time popup"}, 
+	{sTrack.cv_showks, "Shows/Hides KS updates"}, 
+	{sTrack.cv_showtime, "Shows/Hides record time in game HUD"}, 
+}
+
+addHook("ThinkFrame", do
+	if (loaded) then return end
+	
+	-- create/load the config!
+	local config = io.open("stattracker.cfg", "r")
+	if not (config)	-- no config? create one.
+		config = io.open("stattracker.cfg", "w")	-- create it.
+			
+		-- write all our defaults to it.
+		for i = 1, #commands
+			local command = commands[i][1]
+			local command_desc = commands[i][2]
+				
+			config:write(command.name.." "..command.defaultvalue.." // "..command_desc.."\n")
+		end
+			
+		CONS_Printf(consoleplayer, "\x83NOTICE:\x80 stattracker.cfg not found, created a config in \x82/luafiles\x80.")
+		S_StartSound(nil, sfx_bpcrea, consoleplayer)
+	else	-- we got a config, load it!
+		for line in config:lines()
+			COM_BufInsertText(consoleplayer, line)
+		end
+		CONS_Printf(consoleplayer, "\x83NOTICE:\x80 Successfully loaded stattracker.cfg!")
+		S_StartSound(nil, sfx_bpload, consoleplayer)
+	end
+	config:close()
+		
+	-- set this to true, we dont wanna do this again.
+	loaded = true
+end)
