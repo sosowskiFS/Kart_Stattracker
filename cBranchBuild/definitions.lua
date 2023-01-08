1d9ca7ce2a85fce7e4519bcd34f3d1f228bab4f1
Montyblancs <montyblancs99@gmail.com>
2023-01-07 22:26:10 -0500

    Fixes data files not saving if hostmod isn't loaded in

--- cBranchBuild/definitions.lua
+++ cBranchBuild/definitions.lua
@@ -1,5 +1,50 @@
 rawset(_G, "sTrack", {}) -- Stat Tracker global namespace
 
+--The below should stop issues related to consoleplayer not being set properly in dedicated servers
+--Thanks Lighto
+
+-- SRB2Kart Lua Script
+-- consoleplayer -- Get consoleplayer, secondarydisplayplayer,
+-- thirddisplayplayer and fourthdisplayplayer as player_t!
+-- These are always valid too, except possibly in a PlayerCmd hook that was
+-- added before this script.
+--
+-- Copyright 2019 James R.
+-- You may use this script as you please provided that you preserve the above
+-- copyright notice and this text.
+--
+-- P.S. I want to kiss Lat'
+
+if consoleplayer_lua == nil then
+	rawset (_G, "consoleplayer_lua", true)
+
+	local splitplayerids =
+	{
+		"secondarydisplayplayer",
+			"thirddisplayplayer",
+		   "fourthdisplayplayer",
+	}
+
+	addHook ("PlayerCmd", function (p)
+		if (p.splitscreenindex == 0)
+		then
+			rawset (_G, "consoleplayer", p)
+			-- Erase invalidated splitplayers from leaving netgames.
+			for i = 1, 3
+			do
+				if _G[splitplayerids[i]] ~= nil and
+					(not _G[splitplayerids[i]].valid) then _G[splitplayerids[i]] = nil
+				end
+			end
+		else
+			rawset (_G, splitplayerids[p.splitscreenindex], p)
+		end
+	end)
+end -- consoleplayer_lua
+	
+-- the above is valid but i want to die
+-- TyroneSama 2019
+
 sTrack.globalSkinData = {}
 sTrack.globalMapData = {}
 sTrack.globalPlayerData = {}
